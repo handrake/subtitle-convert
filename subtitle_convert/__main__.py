39453c92238019f188a3dcc7253972aeefcea490
Byoung Gi Lee <handrake@gmail.com>
2017-10-10 00:18:11 +0900

    more changes for travis

--- subtitle_convert/__main__.py
+++ subtitle_convert/__main__.py
@@ -1,7 +1,11 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication
-from .subtitle_convert_gui import SubtitleConvertMainDialog
+
+try:
+    from .subtitle_convert_gui import SubtitleConvertMainDialog
+except ImportError:
+    from subtitle_convert.subtitle_convert_gui import SubtitleConvertMainDialog
 
 if __name__ == "__main__":
     APP = QApplication(sys.argv)
