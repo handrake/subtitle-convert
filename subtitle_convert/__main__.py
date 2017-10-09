f0e6920fb4219957eabcd0e64fcbccfa2870964b
Byoung Gi Lee <handrake@gmail.com>
2017-10-10 01:22:51 +0900

    added empty __init__.py for python to recognize subtitle_convert as a proper python module

--- subtitle_convert/__main__.py
+++ subtitle_convert/__main__.py
@@ -2,10 +2,7 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 
-try:
-    from .subtitle_convert_gui import SubtitleConvertMainDialog
-except ImportError:
-    from subtitle_convert.subtitle_convert_gui import SubtitleConvertMainDialog
+from subtitle_convert.subtitle_convert_gui import SubtitleConvertMainDialog
 
 if __name__ == "__main__":
     APP = QApplication(sys.argv)
